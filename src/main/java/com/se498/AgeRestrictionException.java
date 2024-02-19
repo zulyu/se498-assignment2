f2238bc812c2b72e0f322b9ced8cf067d814a8d2
Sergey Sundukovskiy <ssunduko@gmail.com>
2024-02-19 01:13:03 -0800

    Exception Change

--- src/main/java/com/se498/AgeRestrictionException.java
+++ src/main/java/com/se498/AgeRestrictionException.java
@@ -1,5 +1,27 @@
 package com.se498;
 
 public class AgeRestrictionException extends Exception {
+
     //TODO: Implement method(s) for age restriction exception
+    private String date;
+    private String reason;
+
+    public AgeRestrictionException (String date, String reason) {
+    }
+
+    public String getDate() {
+        return null;
+    }
+
+    public void setDate(String date) {
+    }
+
+    public String getReason() {
+
+        return null;
+    }
+
+    public void setReason(String reason) {
+
+    }
 }
